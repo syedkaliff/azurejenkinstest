9e3f893a22aacc01f73e89ab304a6823798c267e
syedkaliff <38420044+syedkaliff@users.noreply.github.com>
2022-08-06 12:57:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,30 @@
-# Terraform Block
 terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.0" 
+    required_providers  {
+        azurerm =   {
+            source  =   "hashicorp/azurerm"
+        }
     }
-    random = {
-      source = "hashicorp/random"
-      version = ">= 3.0"
-    }
-  }
 }
 
 # Provider Block
+
 provider "azurerm" {
- features {}          
+    version         =   "~> 2.0"
+    client_id       =   var.client_id
+    client_secret   =   var.client_secret
+    subscription_id =   var.subscription_id
+    tenant_id       =   var.tenant_id
+    
+    features {}
 }
 
 
-resource "azurerm_resource_group" "example" {
-  name     = "jenkins"
-  location = "West Europe"
+
+
+provider "azuread" {
+    version         =   ">= 0.11"
+    client_id       =   var.client_id
+    client_secret   =   var.client_secret
+    tenant_id       =   var.tenant_id
+    alias           =   "ad"
 }
